627f42f9d3656667cc6f2cc009d4e5314cddfa17
alemendonc <160601834+alemendonc@users.noreply.github.com>
2024-03-10 00:30:05 -0300

    yt5

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-{alert(Esse novo Ensino Médio é uma merda)
+{alert("Esse novo Ensino Médio é uma merda")
 console.log(document.querySelector("#p1)")
 document.querySelector("#p1").style.color="white"
 }
